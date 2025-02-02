07cfff9fbcd5f51f15f60b0b561a94c6f6d3d1f5
William <121023616+Userwr20@users.noreply.github.com>
2025-02-02 12:27:15 +0100

    initialized the function that creates a deck of cards and introduces randomness into it

--- card_game.py
+++ card_game.py
@@ -7,3 +7,10 @@ def draw_card(deck, num_cards):  # this function takes the parameters deck (a li
     hand = [deck.pop() for _ in range (num_cards)] #list comprehension that iterates through num_cards and pops (removes) from the deck and adds it to the hand.
     return hand, deck #this returns the list of drawn cards (hand) and remaining cards (deck)
 
+def create_deck(): #the function creates a full deck of cards (52)
+    suits = ["♥", "♦", "♣", "♠"]
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    deck = [(suit, rank) for suit in suits for rank in ranks]
+    random.shuffle(deck) # this introduces randomness into the deck of cards
+    return deck
+
